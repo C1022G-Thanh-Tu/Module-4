fe076fc50f94517fcce6dc8f0f78112b48ab30ac
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-03-06 08:54:53 +0700

    update

--- ss2_spring_controller/bai_tap/calculator/src/main/java/com/example/controller/CalculatorController.java
+++ ss2_spring_controller/bai_tap/calculator/src/main/java/com/example/controller/CalculatorController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/calculator")
 public class CalculatorController {
@@ -46,5 +50,36 @@ public class CalculatorController {
         model.addAttribute("message", message);
         model.addAttribute("calculationMessage", calculationMessage);
         return "calculator";
+    }
+
+    public static void main(String[] args) {
+        int l =3113;
+        int r = 3223;
+        String[] arr = new String[r-l+1];
+        int j = 0;
+        for (int i = l; i < r+1; i++) {
+            arr[j] = Integer.toString(i);
+            j++;
+        }
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            boolean flag = false;
+            for (int k = 0; k < arr[i].length(); k++) {
+                if (arr[i].charAt(k) == arr[i].charAt(arr[i].length() - k - 1)) {
+                    flag = true;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                strings.add(arr[i]);
+            }
+        }
+        int[] ints = new int[strings.size()];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = Integer.parseInt(strings.get(i));
+        }
+        System.out.println(Arrays.toString(ints));
     }
 }
